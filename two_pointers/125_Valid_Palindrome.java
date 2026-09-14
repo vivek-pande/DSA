bcec8e94b5d3c72d3b04d8f8f101cc42ebc300ac
vivek-pande <pandeyvivek033@gmail.com>
2026-09-14 08:24:41 +0530

    first commit

---
+++ two_pointers/125_Valid_Palindrome.java
@@ -0,0 +1,13 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all 
+non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters
+ and numbers. */
+
+
+public class 123_Valid_palindrome{
+
+    public static void main (String[] args){
+
+        system.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
